d524ae438c1f773e888b6e5e77f81b1a37098840
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-05-11 10:35:09 -0500

    logo done

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Globe from "../globe.png"
 
 export default function Logo() {
     return(
         <header className="journal-logo">
-            <img className="logo-globe" src="src/globe.png" alt="globe"/>
+            <img className="logo-globe" src={Globe} alt="globe"/>
             <span className="logo-text">my travel journal</span>
         </header>
     );
