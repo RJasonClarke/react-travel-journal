fc34efe1e196947d713b25f65211ef3ce3897059
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-05-12 10:22:48 -0500

    done

--- src/data.js
+++ src/data.js
@@ -1,32 +1,32 @@
 export default [
     {
         id: 1,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Machu Picchu",
+        location: "Peru",
+        googleMapsUrl: "https://www.google.com/maps/place/Machu+Picchu/@-13.1631412,-72.5471569,17z/data=!3m1!4b1!4m5!3m4!1s0x916d9a5f89555555:0x3a10370ea4a01a27!8m2!3d-13.1631412!4d-72.5449629",
+        startDate: "March, 2013",
+        endDate: "March, 2013",
+        description: "Machu Picchu is a 15th-century Inca citadel located in the Eastern Cordillera of southern Peru on a 2,430-meter (7,970 ft) mountain ridge. It is located in the Machupicchu District within Urubamba Province above the Sacred Valley, which is 80 kilometers (50 mi) northwest of Cusco. The Urubamba River flows past it, cutting through the Cordillera and creating a canyon with a tropical mountain climate.",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Machu_Picchu%2C_Peru.jpg/1200px-Machu_Picchu%2C_Peru.jpg"
     },
     {
         id: 2,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Tulum",
+        location: "Mexico",
+        googleMapsUrl: "https://www.google.com/maps/place/Tulum+Archaeological+Zone/@20.2149503,-87.4339005,17z/data=!3m1!4b1!4m5!3m4!1s0x8f4fd17d16293265:0xff68d6fca492c963!8m2!3d20.2149504!4d-87.4294212",
+        startDate: "Feb, 2015",
+        endDate: "Feb, 2015",
+        description: "Tulum is the site of a pre-Columbian Mayan walled city which served as a major port for Coba, in the Mexican state of Quintana Roo. The ruins are situated on 12-meter-tall (39 ft) cliffs along the east coast of the Yucatán Peninsula on the Caribbean Sea.",
+        imageUrl: "https://imageio.forbes.com/specials-images/imageserve/5f7df10e35d48e1dde51b79b/Tulum-Mayan-Ruins/960x0.jpg?fit=bounds&format=jpg&width=960"
     },
     {
         id: 3,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Wat Pho",
+        location: "Thailand",
+        googleMapsUrl: "https://www.google.com/maps/place/Wat+Phra+Chetuphon+(Wat+Pho)/@13.7466078,100.4931179,15z/data=!4m5!3m4!1s0x0:0x7df49a08f511c22e!8m2!3d13.7466078!4d100.4931179",
+        startDate: "April, 2015",
+        endDate: "April, 2015",
+        description: "The temple is first on the list of six temples in Thailand classed as the highest grade of the first-class royal temples. It is associated with King Rama I who rebuilt the temple complex on an earlier temple site. It became his main temple and is where some of his ashes are enshrined.",
+        imageUrl: "https://9968c6ef49dc043599a5-e151928c3d69a5a4a2d07a8bf3efa90a.ssl.cf2.rackcdn.com/160281-14.jpg"
     }
 ]
