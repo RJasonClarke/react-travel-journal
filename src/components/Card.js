d87263a9fd5068654bcb7b5d76bad5ceb853a633
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-05-12 09:52:04 -0500

    design completed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
 import React from "react";
+import pin from "../pin.png"
 
 export default function Card(props){
-
-    console.log(props)
-
     return(
         <div className="card">
             <img className="card-img" src={props.item.imageUrl} alt={props.item.title}/>
-            <h4 className="card-location">{props.item.location}</h4>
-            <a className="card-link" href={props.item.googleMapsUrl}></a>
-            <h1 className="card-title">{props.item.title}</h1>
-            <p className="card-start-date">{props.item.startDate}</p>
-            <p className="card-end-date">{props.item.endDate}</p>
-            <p className="card-description">{props.item.description}</p>
+            <div className="right-side">
+                <img src={pin} alt="pin"></img>
+                <span className="card-location"><strong>{props.item.location}</strong></span>
+                <a className="card-link" href={props.item.googleMapsUrl}>View on Google Maps</a>
+                <h1 className="card-title">{props.item.title}</h1>
+                <p className="card-date">{props.item.startDate} - {props.item.endDate}</p>
+                <p className="card-description">{props.item.description}</p>
+            </div>
         </div>
     )
 }
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Globe from "../globe.png"
+import globe from "../globe.png"
 
 export default function Logo() {
     return(
         <header className="journal-logo">
-            <img className="logo-globe" src={Globe} alt="globe"/>
+            <img className="logo-globe" src={globe} alt="globe"/>
             <span className="logo-text">my travel journal</span>
         </header>
     );
